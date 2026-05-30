1dfac84fda86392b0b59068f607135ced703d73d
Nestor Saumeth <nisaumeth@unimagdalena.edu.co>
2026-05-30 07:51:35 -0500

    Ya.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(), tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://52.203.47.163:8080',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      react(), tailwindcss(),
+      babel({ presets: [reactCompilerPreset()] })
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.API_DEV_PROXY,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 })
